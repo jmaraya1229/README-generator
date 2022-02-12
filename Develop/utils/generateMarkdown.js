9b01b957a53dc9ee7eccd4941471a3093a9975d0
jmaraya1229 <jessemaraya2013@gmail.com>
2022-02-12 10:08:36 -0800

    reformatted template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,39 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
   let tempToC = `\n## Table of Contents`;
   if (data.installation !== '') {
-    tempToC += `
-    \n* [Installation](#installaion)`
+    tempToC += `\n* [Installation](#installation)`
   };
 
   if (data.usage !== '') {
-    tempToC += `
-    \n* [Usage](#usage)`
+    tempToC += `\n* [Usage](#usage)`
   };
 
   if (data.contribution !== '') {
-    tempToC += `
-    \n* [Contribution](#contribution)`
+    tempToC += `\n* [Contribution](#contribution)`
   };
 
   if (data.test !== '') {
-    tempToC += `
-    \n* [Tests](#tests)`
+    tempToC += `\n* [Tests](#tests)`
   };
-
 
   let tempmd = 
   `# ${data.title}
@@ -43,48 +45,35 @@ function generateMarkdown(data) {
   
   tempmd += tempToC;
 
-  // tempmd += ` *[License](#license)`;
-
   if (data.install !== '') {
-    tempmd += `
-    \n## Installation
-    \n${data.install}
-    `
+    tempmd += 
+      `\n## Installation
+      \n${data.install}`
   };
 
   if (data.usage !== '') {
-    tempmd += `
-    \n## Usage
-    \n${data.usage}
-    `
+    tempmd += 
+      `\n## Usage
+      \n${data.usage}`
   };
 
   if (data.contribution !== '') {
-    tempmd += `
-    \n## Contribution
-    \n${data.contribution}
-    `
+    tempmd += 
+      `\n## Contribution
+      \n${data.contribution}`
   };
 
   if (data.test !== '') {
-    tempmd += `
-    \n## Tests
-    \n${data.test}
-    `
+    tempmd += 
+      `\n## Tests
+      \n${data.test}`
   };
 
-  let qinfo = `
-  \n## Questions
-  Feel free to contact me for any questions!
-  * Github: [@${data.github}](https://github.com/${data.github})
-  `;
-
-  let emailmd = `* Email: ${data.email}`;
-  if (data.email !== null) {
-    qinfo += emailmd
-  }
-
-  
+  let qinfo = 
+    `\n## Questions
+    \nFeel free to contact me for any questions!
+    \n* Github: [@${data.github}](https://github.com/${data.github})
+    \n* Email: ${data.email}`;
   
   tempmd += qinfo;
 

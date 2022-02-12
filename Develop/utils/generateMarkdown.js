f2f84e8895580028a96c6ba5ad95768c7e88df63
jmaraya1229 <jessemaraya2013@gmail.com>
2022-02-11 16:59:35 -0800

    edited md template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,31 +12,83 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-  ## Description
-  ${data.description}
+  let tempToC = `\n## Table of Contents`;
+  if (data.installation !== '') {
+    tempToC += `
+    \n* [Installation](#installaion)`
+  };
 
-  ## Table of Contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Contribution](#contribution)
-  * [Questions](#questions)
+  if (data.usage !== '') {
+    tempToC += `
+    \n* [Usage](#usage)`
+  };
 
-  ## Installation
-  ${data.install}
+  if (data.contribution !== '') {
+    tempToC += `
+    \n* [Contribution](#contribution)`
+  };
 
-  ## Usage
-  ${data.usage}
+  if (data.test !== '') {
+    tempToC += `
+    \n* [Tests](#tests)`
+  };
 
-  ## Contribution
-  ${data.contribution}
 
-  ## Questions
-  Feel free to utilize this app to create a README for your project. Contact me with examples or any questions via the information below:
+  let tempmd = 
+  `# ${data.title}
+  \n## Description
+  \n${data.description}
+  `
+  
+  tempmd += tempToC;
+
+  // tempmd += ` *[License](#license)`;
+
+  if (data.install !== '') {
+    tempmd += `
+    \n## Installation
+    \n${data.install}
+    `
+  };
+
+  if (data.usage !== '') {
+    tempmd += `
+    \n## Usage
+    \n${data.usage}
+    `
+  };
+
+  if (data.contribution !== '') {
+    tempmd += `
+    \n## Contribution
+    \n${data.contribution}
+    `
+  };
+
+  if (data.test !== '') {
+    tempmd += `
+    \n## Tests
+    \n${data.test}
+    `
+  };
+
+  let qinfo = `
+  \n## Questions
+  Feel free to contact me for any questions!
   * Github: [@${data.github}](https://github.com/${data.github})
-  * Email: ${data.email}
-`;
-}
+  `;
+
+  let emailmd = `* Email: ${data.email}`;
+  if (data.email !== null) {
+    qinfo += emailmd
+  }
+
+  
+  
+  tempmd += qinfo;
+
+  return tempmd;
+};
 
 module.exports = generateMarkdown;
